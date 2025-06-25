1297a9833f3075469746156d4746b2be3ce715e9
Ben Cooper <ben.f.cooper@gmail.com>
2025-06-25 22:39:59 +0100

    feat: ✨ cute css

--- public/static/scripts/app.js
+++ public/static/scripts/app.js
@@ -1,26 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("a.nav-link").forEach(link => {
-        link.addEventListener("click", (event) => {
-            event.preventDefault();
-            const url = link.href;
-
-            if (!document.startViewTransition) {
-                window.location.href = url; // Fallback for unsupported browsers
-                return;
-            }
-
-            document.startViewTransition(() => {
-                return new Promise(resolve => {
-                    fetch(url)
-                        .then(response => response.text())
-                        .then(html => {
-                            const parser = new DOMParser();
-                            const doc = parser.parseFromString(html, "text/html");
-                            document.body.innerHTML = doc.body.innerHTML;
-                            resolve();
-                        });
-                });
-            });
-        });
-    });
+    console.info("Welcome to my site!")
 });
